55c1ec69c374b0190bf6d855ed83d1f6e660a524
kiasf159 <40749857+kiasf159@users.noreply.github.com>
2019-01-21 14:59:42 +0900

    Create BankingDeclare.h

---
+++ OOP Project with Cpp/Phase 8/BankingDeclare.h
@@ -0,0 +1,21 @@
+#ifndef __BANKINGDECLARE_H__
+#define __BANKINGDECLARE_H__
+
+#include <iostream>
+#include <cstring>
+#pragma warning(disable : 4996)
+using namespace std;
+const int NAME_LEN = 20;
+
+// Select
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+//Credit Grade
+enum { LEVEL_A = 7, LEVEL_B = 4, LEVEL_C = 2 };
+
+//Type of Account
+enum { NORMAL = 1, CREDIT = 2 };
+
+#endif // !__BANKINGDECLARE_H__
+
+// same of previous version
