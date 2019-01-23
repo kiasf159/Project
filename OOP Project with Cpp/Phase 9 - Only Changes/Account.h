4c576fd062caddb0c9f60eae6cf32589e1afb1d1
kiasf159 <40749857+kiasf159@users.noreply.github.com>
2019-01-23 21:58:40 +0900

    Create Account.h

---
+++ OOP Project with Cpp/Phase 9 - Only Changes/Account.h
@@ -0,0 +1,21 @@
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+// Entity Class, Top
+#include "String.h"
+
+class Account
+{
+private:
+	int accID;
+	int balance;
+	String cusName;
+public:
+	Account(int ID, int money, String name);
+	int GetAccID() const;
+	virtual void Deposit(int money);
+	int Withdraw(int money);
+	void ShowAccInfo() const;
+};
+
+#endif
