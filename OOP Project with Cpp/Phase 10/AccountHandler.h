2e4c3487df58c3661a65b09ebc431abf4d1176d5
kiasf159 <40749857+kiasf159@users.noreply.github.com>
2019-01-26 15:25:29 +0900

    Create AccountHandler.h

---
+++ OOP Project with Cpp/Phase 10/AccountHandler.h
@@ -0,0 +1,25 @@
+#ifndef __ACCOUNT_HANDLER_H__
+#define __ACOOUNT_HANDLER_H__
+
+#include "Account.h"
+#include "BoundCheckArray.h"
+
+class AccountHandler
+{
+private:
+	BoundCheckArray<Account*> accArr;
+	int accNum;
+public:
+	AccountHandler();
+	void ShowMenu() const;
+	void MakeAccount();
+	void DepositMoney();
+	void WithdrawMoney();
+	void ShowAllAccInfo() const;
+	~AccountHandler();
+protected:
+	void MakeNormalAccount();
+	void MakeCreditAccount();
+};
+
+#endif // !__ACCOUNT_HANDLER_H__
